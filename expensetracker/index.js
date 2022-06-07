62f9de893b5839e79f8c2cdf7acfa300f95f0870
unknown <kalsijatin67@gmail.com>
2022-06-06 23:22:28 -0400

    dome

--- expensetracker/index.js
+++ expensetracker/index.js
@@ -1,7 +1,35 @@
-const remBtn = document.querySelector('.remove')
+let name = document.querySelector('.name-field')
+let date = document.querySelector('.date-field')
+let amount = document.querySelector('.amount-field')
+let table = document.querySelector('.items')
+const addBtn = document.querySelector('.add-btn')
 
-function SomeDeleteRowFunction(o) {
-    var p = o.parentNode
-    p.parentNode.removeChild(p)
+addBtn.addEventListener('click', add)
+
+function add() {
+    let newRow = document.createElement('tr')
+    let newNameCol = document.createElement('td')
+    let newDateCol = document.createElement('td')
+    let newAmountCol = document.createElement('td')
+    let newBtnCol = document.createElement('td')
+    let newBtn = document.createElement('button')
+    newBtn.className = 'remove'
+    newBtn.innerHTML = 'X'
+    newBtn.setAttribute('onclick', 'rem(this)')
+    newBtnCol.appendChild(newBtn)
+    newNameCol.textContent = name.value
+    newDateCol.textContent = date.value
+    newAmountCol.textContent = '$' + amount.value
+    if (name.value && date.value && amount.value) {
+        newRow.appendChild(newNameCol)
+        newRow.appendChild(newDateCol)
+        newRow.appendChild(newAmountCol)
+        newRow.appendChild(newBtnCol)
+        table.appendChild(newRow)
+    }
 }
 
+function rem(o) {
+    var p = o.parentNode.parentNode
+    p.parentNode.removeChild(p)
+}
